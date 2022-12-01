e37621f5d09a1cb83632a8a1c1652c7eba65f5d0
Jet Milhomme <milhommejet@yahoo.com>
2022-12-01 15:45:28 -0800

    -Added variable to Person class -Added account list to Person class -Added constructer for Person class -Created person and pushed ex. account for testing

--- Banking Application/Person.cs
+++ Banking Application/Person.cs
@@ -8,5 +8,17 @@ namespace Banking_Application
 {
     internal class Person
     {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public List<Account> Accounts = new List<Account>();
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
     }
 }
--- Banking Application/Program.cs
+++ Banking Application/Program.cs
@@ -8,7 +8,10 @@ namespace Banking_Application
         [STAThread]
         static void Main()
         {
+            Person person1 = new Person("aaa", "bbb", 69);
             Account acc1 = new Account(123456789, "asdfasdf");
+
+            person1.Accounts.Add(acc1);
 
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
