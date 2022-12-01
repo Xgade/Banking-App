79e0c4b0bc42659812f45fda0e62a68fa74665d8
Jet Milhomme <milhommejet@yahoo.com>
2022-12-01 15:30:47 -0800

    Add project files.

---
+++ Banking Application/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Application
+{
+    internal class Account
+    {
+        int accountNum;
+        string accountOwner; // Person class
+    }
+}
