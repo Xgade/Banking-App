f9d8fef801e49f23573ebdad9c62ef0854c23ad5
Jet Milhomme <milhommejet@yahoo.com>
2022-12-01 16:38:33 -0800

    - Added List Box (display for accounts in list) - Added example accounts in Form1.cs - Added for loop to auto add person1* account nums - Added Label (Form1.Designer)

--- Banking Application/Form1.Designer.cs
+++ Banking Application/Form1.Designer.cs
@@ -1,4 +1,10 @@
-﻿namespace Banking_Application
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Application
 {
     partial class Form1
     {
@@ -28,12 +34,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AccountNumbers = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AccountNumbers
+            // 
+            this.AccountNumbers.AutoSize = true;
+            this.AccountNumbers.Location = new System.Drawing.Point(20, 20);
+            this.AccountNumbers.Name = "AccountNumbers";
+            this.AccountNumbers.Size = new System.Drawing.Size(67, 15);
+            this.AccountNumbers.TabIndex = 0;
+            this.AccountNumbers.Text = "Account #s";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AccountNumbers);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label AccountNumbers;
     }
 }
--- Banking Application/Form1.cs
+++ Banking Application/Form1.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Banking_Application
 {
     public partial class Form1 : Form
@@ -5,6 +11,28 @@ namespace Banking_Application
         public Form1()
         {
             InitializeComponent();
+            ListBox accountList = new ListBox();
+            accountList.Location = new System.Drawing.Point(20, 40);
+            accountList.Name = "AccountList";
+            accountList.Size = new System.Drawing.Size(200, 200);
+            accountList.BackColor = System.Drawing.Color.White;
+            accountList.ForeColor = System.Drawing.Color.Black;
+            accountList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            Controls.Add(accountList);
+
+            Person person1 = new Person("aaa", "bbb", 69);
+            Account acc1 = new Account(123456789, "Acc1 Owner");
+            Account acc2 = new Account(24681012, "Acc2 Owner");
+            Account acc3 = new Account(36912151, "Acc3 Owner");
+
+            person1.Accounts.Add(acc1);
+            person1.Accounts.Add(acc2);
+            person1.Accounts.Add(acc3);
+
+            for(int i = 0; i < person1.Accounts.Count; i++)
+            {
+                accountList.Items.Add(person1.Accounts[i].accountNum);
+            }
         }
     }
 }
